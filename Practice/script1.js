dae2f88c58400caadb7d8940eba1d49d5cdd306c
dharmik tank <tankdharmik123@gmail.com>
2021-01-08 20:23:02 +0530

    final commit

---
+++ Practice/script1.js
@@ -0,0 +1,17 @@
+var firstName='john';
+var age=29;
+console.log(firstName +' '+age);
+
+var job,isMarried;
+job='teacher';
+isMarried=false;
+
+console.log(firstName + ' is a '+ age + ' year old ' + job + ' isMarried ? '+ isMarried);
+
+age="twenty eight";
+job='carpenter';
+
+alert(firstName + ' is a '+ age + ' year old ' + job + ' isMarried ? '+ isMarried);
+
+var city=prompt(" what is your city ? ");
+console.log(firstName + ' from ' + city);
