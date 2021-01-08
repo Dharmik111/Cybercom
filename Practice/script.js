be429d46ed0d4885ef4155521d36fcdf5bf6d9f6
dharmik tank <tankdharmik123@gmail.com>
2021-01-08 20:18:47 +0530

    practice work

---
+++ Practice/script.js
@@ -0,0 +1,16 @@
+console.log("Hello World!!!");
+
+var firstName='john';
+console.log(firstName);
+
+var lastName='smith';
+var age='29';
+
+var fullage=true;
+console.log(fullage); 
+
+var job;
+console.log(job);
+
+job='teacher';
+console.log(job);
---
+++ Practice/script2.js
@@ -0,0 +1,26 @@
+
+//math operators
+
+var now,yeardavid,yearmonty;
+now=2021;
+agedavid=29;
+agemonty=45;
+yeardavid=now-agedavid;
+yearmonty=now-agemonty;
+
+console.log(yeardavid);
+console.log(now+2);
+console.log(now*2);
+console.log(now/10);
+
+//logical operator
+
+var davidOlder= agedavid > agemonty;
+console.log(davidOlder);
+
+//type of operator
+
+console.log(typeof davidOlder);
+console.log(typeof agemonty);
+console.log(typeof 'david is big bro');
+console.log(typeof x);
